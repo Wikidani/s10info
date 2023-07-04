c90017ceb2fe9ab425093b37a5380e336b91b3eb
Daniel <danielhonzawa@hotmail.com>
2023-07-04 13:53:22 -0300

    added timeline with svg bulletpoints, kinda proud of it

--- components/card.js
+++ components/card.js
@@ -1,33 +1,39 @@
 import React, { useEffect } from 'react';
-import styles from "./styles.module.css";
+import styles from "./card.module.css";
 import utilStyles from '../styles/utils.module.css';
+import Bullet from './bullet';
 
 const Card = ({ date, event, location, packageData }) => {
+  var a = packageData[0].eventos
   function showArray() {
-    console.log(packageData);
+    console.log(packageData[0].evento);
   }
-
   return (
-    <div className={styles.container}>
-      <div className={`${styles.step} ${styles.completed}`}>
-        <div className={styles["v-stepper"]}>
-          <div className={styles.circle}></div>
-          <div className={styles.line}></div>
-        </div>
-        <div className={styles.content}>
-          blah blah blah
-        </div>
-      </div>
-       <div className={`${styles.step}`}>
-        <div className={styles["v-stepper"]}>
-          <div className={styles.circle}></div>
-          <div className={styles.line}></div>
-        </div>
-        <div className={styles.content}>
-          blah blah blah
-        </div>
-      </div>
-    </div>
+    <>
+      <ul className={styles.list}>
+        <li>
+          <div className={`${styles.bullet} ${styles.big}`}>
+            <svg aria-hidden="true" viewBox="0 0 32 32" focusable="false">
+              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+              <circle cx="16" cy="16" r="6"></circle>
+            </svg>
+          </div>
+          {packageData[0].evento}
+        </li>
+        {packageData.slice(1).map((item, index) => {
+          //console.log(item);
+          if(packageData.length === 1 ){
+            return 
+          }
+          return (
+            <li key={index}>
+              <Bullet content={item}></Bullet> {/* Render the <Bullet> component for each item */}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+
   );
 };
 
