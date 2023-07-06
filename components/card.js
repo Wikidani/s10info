361148f090fe30d4f34d14afc20e1bb74f5a6d20
Daniel <danielhonzawa@hotmail.com>
2023-07-06 16:19:48 -0300

    mvp done, user can search for his parcel and see it on a timeline

--- components/card.js
+++ components/card.js
@@ -12,22 +12,17 @@ const Card = ({ date, event, location, packageData }) => {
     <>
       <ul className={styles.list}>
         <li>
-          <div className={`${styles.bullet} ${styles.big}`}>
-            <svg aria-hidden="true" viewBox="0 0 32 32" focusable="false">
-              <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
-              <circle cx="16" cy="16" r="6"></circle>
-            </svg>
-          </div>
-          {packageData[0].evento}
+          <Bullet content={packageData[0].evento} isLastItem={true} ></Bullet> {/* Render the <Bullet> component for each item */}
+
         </li>
         {packageData.slice(1).map((item, index) => {
           //console.log(item);
-          if(packageData.length === 1 ){
-            return 
+          if (packageData.length === 1) {
+            return
           }
           return (
             <li key={index}>
-              <Bullet content={item}></Bullet> {/* Render the <Bullet> component for each item */}
+              <Bullet content={item} isLastItem={false} ></Bullet> {/* Render the <Bullet> component for each item */}
             </li>
           );
         })}
