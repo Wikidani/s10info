a2455dd3811ba01cd6f641e8e7438955eb6bb669
Daniel <danielhonzawa@hotmail.com>
2023-07-07 16:12:49 -0300

    added back button lol

--- components/card.js
+++ components/card.js
@@ -1,15 +1,32 @@
 import React, { useEffect } from 'react';
 import styles from "./card.module.css";
-import utilStyles from '../styles/utils.module.css';
 import Bullet from './bullet';
+import Image from 'next/image'
+import homeIcon from '../public/home.png'
+import Head from 'next/head'
+import { useRouter } from 'next/router';
 
 const Card = ({ date, event, location, packageData }) => {
-  var a = packageData[0].eventos
-  function showArray() {
-    console.log(packageData[0].evento);
+  const router = useRouter();
+  function goBack() {
+    router.push({
+      pathname: '/',
+      //packageId: { inputContent },
+    });
   }
   return (
     <>
+      <Head>
+        <title> Trackin your stuff </title>
+      </Head>
+      <Image
+        src={homeIcon}
+        width={24}
+        height={24}
+        alt="Home icon"
+        onClick={goBack}
+      />
+
       <ul className={styles.list}>
         <li>
           <Bullet content={packageData[0].evento} isLastItem={true} ></Bullet> {/* Render the <Bullet> component for each item */}
